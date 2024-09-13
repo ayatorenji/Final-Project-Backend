94f52812a7553a89f11bbf4705e92ba7bb372c0c
Chayanon <koonjjkung.castgame@gmail.com>
2024-09-13 15:58:23 +0700

    Add testing database

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports ={
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "project",
+    HOST: "b8yk85cjetmrjczebhow-mysql.services.clever-cloud.com",
+    USER: "uczirkppf2pelurm",
+    PASSWORD: "4QoIQ0eR1gOx3GzjHi4g",
+    DB: "b8yk85cjetmrjczebhow",
     PORT: "3306",
     // HOST: "sql.freedb.tech",
     // USER: "freedb_user_api",
