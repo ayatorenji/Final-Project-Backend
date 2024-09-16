be022dc8b6d7c82ef4b684144f79a9d70c397524
Chayanon <koonjjkung.castgame@gmail.com>
2024-09-16 13:44:14 +0700

    Debug server and db

--- server.js
+++ server.js
@@ -3,27 +3,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require('path');
-
 const app = express();
+
+// Global directory
 global.__basedir = __dirname;
 
-// CORS options
-const corsOptions = { origin: "*" };
-app.use(cors(corsOptions));
-
-// Body parser middleware
+// Middleware
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve static assets
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-// Base route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to the Stray Animal Care API." });
-});
-
-// Include route files
+// Routes
 require("./app/routes/user.routes")(app);
 require("./app/routes/file.routes")(app);
 require("./app/routes/post.routes")(app);
@@ -31,12 +22,13 @@ require("./app/routes/animalLife.routes")(app);
 require("./app/routes/chat.routes")(app);
 require("./app/routes/mapLocation.routes")(app);
 
-// Set up the server
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Default route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the Stray Animal Care API." });
 });
 
-// Set server timeouts
-server.keepAliveTimeout = 30 * 1000;
-server.headersTimeout = 35 * 1000;
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
