53aedde766b7edcc5894f77aeafeed7a61ccb609
BIGGY00 <bowbigzaza2014@gmail.com>
2024-09-22 21:14:56 +0700

    Update database config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-module.exports ={
-    HOST: "b8yk85cjetmrjczebhow-mysql.services.clever-cloud.com",
-    USER: "uczirkppf2pelurm",
-    PASSWORD: "4QoIQ0eR1gOx3GzjHi4g",
-    DB: "b8yk85cjetmrjczebhow",
-    PORT: "3306",
+module.exports = {
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DB,
+    PORT: process.env.PORT
 };
