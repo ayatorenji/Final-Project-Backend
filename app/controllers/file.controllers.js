5ab3cfb73e58b333b578b1b89ee8c4698d8dc24e
Chayanon <koonjjkung.castgame@gmail.com>
2024-10-10 03:52:32 +0700

    Change upload image method

--- app/controllers/file.controllers.js
+++ app/controllers/file.controllers.js
@@ -1,16 +1,18 @@
-const uploadFile = require("../middleware/upload");
+const { uploadMiddleware, uploadToFirebase } = require("../middleware/upload");
+
 const uploadController = async (req, res)=>{
     try {
         console.log("file controller")
-        await uploadFile(req, res);
+        await uploadMiddleware(req, res);
         if(req.file == undefined){
             return res.status(400).send({
                 message: "Not Found the upload file."
             });
         }
+        const fileUrl = await uploadToFirebase(req.file);
         res.status(200).send({
-            message: "Upload file successfully: " + req.file.filename,
-            uploadFileName: req.file.filename
+            message: "Upload file successfully.",
+            fileUrl: fileUrl // Return the Firebase file URL
         });
     } catch (error) {
         res.status(500).send({ 
@@ -21,12 +23,32 @@ const uploadController = async (req, res)=>{
 
 const displayAvatar = (req, res)=>{
     const filename = req.params.name;
-    const directoryPath = __basedir + "/assets/";
-    console.log("a:" + directoryPath)
-    res.download(directoryPath + filename, filename, (err)=>{
-        if(err){
-            res.status(500).send({ message: "Could not display the file. " + err});
-        }
-    });
+    const publicUrl = `https://storage.googleapis.com/final-project-142d2.appspot.com/${filename}`;
+    res.redirect(publicUrl); // Redirect to the Firebase URL
+    // const directoryPath = __basedir + "/assets/";
+    // console.log("a:" + directoryPath)
+    // res.download(directoryPath + filename, filename, (err)=>{
+    //     if(err){
+    //         res.status(500).send({ message: "Could not display the file. " + err});
+    //     }
+    // });
 };
+
+exports.uploadSingleFile = async (req, res) => {
+    try {
+      if (!req.file) {
+        return res.status(400).send("No file uploaded.");
+      }
+  
+      // Upload file to Firebase and get the download URL
+      const downloadURL = await uploadFileToFirebase(req.file);
+  
+      res.status(200).json({ uploadFileName: downloadURL }); // Make sure you return the Firebase URL here
+    } catch (error) {
+      res.status(500).json({ message: "File upload failed", error: error.message });
+    }
+  };
+
+  console.log("Uploading file:", req.file);
+  
 module.exports = { uploadController, displayAvatar};
--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -1,18 +1,57 @@
 //upload.js
-const multer = require("multer")
-const util = require("util")
-const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, __basedir + "/assets/");
-    },
-    filename: (req, file, cb)=>{
-        const extArray = file.mimetype.split("/");
-        const extension = extArray[extArray.length-1];
-        const newFilename = "Fileupload-"+Date.now()+"."+extension;
-        cb(null, newFilename);
-    }
+const admin = require("firebase-admin");
+const multer = require("multer");
+const path = require("path");
+const util = require("util");
+const serviceAccount = require("../final-project-142d2-firebase-adminsdk-9bw96-0693c9e497.json");
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "final-project-142d2.appspot.com" // Replace with your Firebase project bucket
 });
-const uploadFile = multer({ storage: storage}).single("singlefile");
+
+const bucket = admin.storage().bucket();
+
+// Multer configuration
+const storage = multer.memoryStorage(); // Store the file in memory
+const uploadFile = multer({ storage: storage }).single("singlefile");
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb)=>{
+//         cb(null, __basedir + "/assets/");
+//     },
+//     filename: (req, file, cb)=>{
+//         const extArray = file.mimetype.split("/");
+//         const extension = extArray[extArray.length-1];
+//         const newFilename = "Fileupload-"+Date.now()+"."+extension;
+//         cb(null, newFilename);
+//     }
+// });
+// const uploadFile = multer({ storage: storage}).single("singlefile");
+
 //promisify => async-await
 const uploadMiddleware = util.promisify(uploadFile);
-module.exports = uploadMiddleware;
+
+// Upload to Firebase function
+const uploadToFirebase = async (file) => {
+    const fileName = `Fileupload-${Date.now()}${path.extname(file.originalname)}`;
+    const blob = bucket.file(fileName);
+    const blobStream = blob.createWriteStream({
+        metadata: {
+            contentType: file.mimetype
+        }
+    });
+
+    return new Promise((resolve, reject) => {
+        blobStream.on('error', (error) => reject(error));
+        blobStream.on('finish', async () => {
+            // Generate URL for accessing the file
+            const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+            resolve(publicUrl);
+        });
+        blobStream.end(file.buffer);
+    });
+};
+
+module.exports = { uploadMiddleware, uploadToFirebase };
