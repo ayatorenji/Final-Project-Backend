ff62cbd0e8eb62f91bde8062d547b8ca1c74226c
Chayanon <koonjjkung.castgame@gmail.com>
2024-12-03 04:57:07 +0700

    123456

--- app/models/comment.model.js
+++ app/models/comment.model.js
@@ -23,10 +23,9 @@ Comment.create = (newComment, result) => {
 Comment.findBySubPostId = (subPostId, result) => {
     sql.query(
         `SELECT comments.comment_id, comments.ment, comments.created_at, 
-        users.fullname AS author, users.img AS user_img, animal_life.image as sub_post_image
+        users.fullname AS author, users.img AS user_img
         FROM comments 
         JOIN users ON comments.user_id = users.id 
-        JOIN animal_life ON comments.sub_post_id = animal_life.id
         WHERE comments.sub_post_id = ? 
         ORDER BY comments.created_at DESC`,
         [subPostId],
