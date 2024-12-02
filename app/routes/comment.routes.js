ee6521df7f30cf27d2a85e05cada69c53fedf24e
Chayanon <koonjjkung.castgame@gmail.com>
2024-12-03 03:58:35 +0700

    debug

--- app/routes/comment.routes.js
+++ app/routes/comment.routes.js
@@ -3,7 +3,7 @@ module.exports = app => {
     const comment_controller = require("../controllers/comment.controller.js");
     var router = require("express").Router();
 
-    router.post("/", comment_controller.create);
+    router.post("/:subPostId/comment", comment_controller.create);
     router.get("/:subPostId", comment_controller.findBySubPostId);
 
     app.use("/api/comments", router);
