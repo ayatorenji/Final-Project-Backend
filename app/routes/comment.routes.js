8bbbfeea37e98270e41d6b15d95b761690b53d96
Chayanon <koonjjkung.castgame@gmail.com>
2024-12-03 03:37:59 +0700

    fix variable not matched

--- app/routes/comment.routes.js
+++ app/routes/comment.routes.js
@@ -1,10 +1,10 @@
 // comment.routes.js
 module.exports = app => {
     const comment_controller = require("../controllers/comment.controller.js");
-    const router = require("express").Router();
+    var router = require("express").Router();
 
     router.post("/", comment_controller.create);
-    router.get("/:subPostId", comment_controller.findByPostId);
+    router.get("/:subPostId", comment_controller.findBySubPostId);
 
     app.use("/api/comments", router);
 };
